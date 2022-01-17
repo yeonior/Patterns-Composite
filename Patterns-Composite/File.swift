597f663b99c66a761edb8c3ec5488b526a6b4384
Rus <de.mr44@gmail.com>
2022-01-17 17:12:56 +0500

    Added File class

---
+++ Patterns-Composite/File.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  Patterns-Composite
+//
+//  Created by Ruslan on 17.01.2022.
+//
+
+import Foundation
+
+class File: Composite {
+    
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func showContent() -> Any {
+        name
+    }
+    
+    func addComponent(_ component: Composite) {
+        print("Cannot add a folder")
+    }
+    
+    func countContent() -> Int {
+        1
+    }
+}
